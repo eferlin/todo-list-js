8a34b98ecf8e40880edd8dbdf16c1f01593e1a32
eferlin <edgarferlin@gmail.com>
2022-06-12 21:21:33 -0300

    3 config routes

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const path = require("path");
+const routes = require("./routes/routes");
 
 const app = express();
 const port = 3000;
 
-app.set("view engine", "ejs")
-app.use(express.static(path.join(__dirname,"public")));
-// ROTA
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
 
 app.listen(3000, () =>
   console.log(`Servidor rodando em HTTP://localhost:${port}`)
--- public/script.js
+++ public/script.js
@@ -1,2 +1,1 @@
 alert("fala quengaaa");
-alert("chupaa");
